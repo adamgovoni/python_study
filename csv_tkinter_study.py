e5757ab299a31374126048b74a5250658a907c1a
adamgovoni <93485513+adamgovoni@users.noreply.github.com>
2023-07-27 17:22:28 -0400

    input box
    changed from terminal input to input box which writes to csv upon new data button click

--- csv_tkinter_study.py
+++ csv_tkinter_study.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import csv
+from tkinter import messagebox
 
 def on_new_data():
 	# function to handle the "New data" button click
@@ -17,7 +18,7 @@ def on_new_data():
 
 	file_name = "data.csv"
 	# ask the user for input
-	user_data = input("Enter your data (comma-separated): ").split(',')
+	user_data = input_box.get().split(',')
 	write_to_csv(file_name, user_data)
 	# displayed in terminal that the button was clicked
 	print("New data button clicked!")
@@ -31,9 +32,9 @@ def on_show_data():
 	with open('data.csv', 'r') as f:
 		reader = csv.reader(f)
 
-	# read file row by row
-	for row in reader:
-		print(row)
+		# read file row by row
+		for row in reader:
+			print(row)
 	# displayed in terminal that the button was clicked
 	print("Show data button clicked!")
 
@@ -41,11 +42,15 @@ if __name__ == "__main__":
 	# create the main window
 	root = tk.Tk()
 	root.title("Button CSV Example")
-
+	root.geometry("400x300")
+	# Create an input box
+	input_box = tk.Entry(root)
+	input_box.pack(padx=10, pady=10)
+	
 	# create the "New Data" button
 	new_data_button = tk.Button(root, text="New Data", command=on_new_data)
 	new_data_button.pack(pady=10)
-
+	
 	# create the "Show Data" button
 	show_data_button = tk.Button(root, text="Show Data", command=on_show_data)
 	show_data_button.pack(pady=10)
