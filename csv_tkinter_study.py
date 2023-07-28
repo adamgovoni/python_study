5ba99e90ee70d74d9bf6d5e2388b1e5396208a0e
mxninja17 <agovoni@outlook.com>
2023-07-27 20:05:12 -0400

    added text box to show output instead of in terminal

--- csv_tkinter_study.py
+++ csv_tkinter_study.py
@@ -13,38 +13,51 @@ def on_new_data():
 			csv_writer = csv.writer(csvfile)
 			csv_writer.writerow(data)
 
-		print("Data has been written to the CSV file.")
-
+		confirmation = "Data has been written to the CSV file."
+		
+		# text box cleared for new data
+		text_box.delete(1.0, tk.END)
+		text_box.insert(tk.END, confirmation)
 
 	file_name = "data.csv"
+	
 	# ask the user for input
 	user_data = input_box.get().split(',')
+
+	# clear the input box
+	input_var.set('')
 	write_to_csv(file_name, user_data)
-	# displayed in terminal that the button was clicked
-	print("New data button clicked!")
+	
 
 def on_show_data():
 	# function to handle the "Show data" button click
 
 	# add your logic here for what should happen when the button is clicked
-	# to read a csv file into the terminal
+	# to read a csv file
 	# open file
 	with open('data.csv', 'r') as f:
 		reader = csv.reader(f)
 
+		# clear text box for new data
+		text_box.delete(1.0, tk.END)
+
 		# read file row by row
 		for row in reader:
-			print(row)
+			text_box.insert(tk.END, row)
+
 	# displayed in terminal that the button was clicked
-	print("Show data button clicked!")
+	show_confirmation= "Show data button clicked!"
+	text_box.insert(tk.END, show_confirmation)
 
 if __name__ == "__main__":
 	# create the main window
 	root = tk.Tk()
 	root.title("Button CSV Example")
 	root.geometry("400x300")
-	# Create an input box
-	input_box = tk.Entry(root)
+	
+	# create an input box
+	input_var = tk.StringVar()
+	input_box = tk.Entry(root, textvariable=input_var)
 	input_box.pack(padx=10, pady=10)
 	
 	# create the "New Data" button
@@ -54,6 +67,10 @@ if __name__ == "__main__":
 	# create the "Show Data" button
 	show_data_button = tk.Button(root, text="Show Data", command=on_show_data)
 	show_data_button.pack(pady=10)
+
+	# create text box
+	text_box = tk.Text(root, height=10, width=50)
+	text_box.pack(pady=10)
 
 	# start the tkinter main loop
 	root.mainloop()
